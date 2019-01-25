a16eb3404a188ff925a8df1a912de2df13d9a045
Connor <cnnrznn@udel.edu>
2019-01-24 19:05:01 -0500

    validate messages, only allowed to receive one message of a given type from the same id for the same round

--- src/clj_net/broadcast.clj
+++ src/clj_net/broadcast.clj
@@ -1,27 +1,33 @@
 (ns clj-net.broadcast
-  (require [clj-net.core :refer :all]
+  (require [clj-net [core :refer :all]
+                    [util :as util]]
            [clojure.pprint :as pp]))
 
-(defn p1
+(defn phase1
   [addrs]
   (loop [msgs #{}]
     (pp/pprint msgs)
-    (recur (conj msgs (orecv)))))
+    (recur (conj msgs (util/validate msgs (orecv))))))
 
 (defn bracha-broadcast
   ([addrs obj]
     (obroadcast addrs obj)
     (bracha-broadcast addrs))
-  ([addrs]))
+  ([addrs]
+    (let [m1 (phase1 addrs)]
+      m1)))
 
 (defn -main
-  []
-  (let [addrs [{:host "localhost" :port 3333}
-               {:host "localhost" :port 3333}
-               {:host "localhost" :port 3333}
-               {:host "localhost" :port 3333}]]
-    (obroadcast addrs {:type :initial :v 0})
-    (obroadcast addrs {:type :initial :v 1})
-    (obroadcast addrs {:type :echo :v 0})
-    (obroadcast addrs {:type :initial :v 1})
-    (p1 addrs)))
+  ([si v]
+    (let [addrs [{:host "" :port 3333}
+                 {:host "" :port 3333}
+                 {:host "" :port 3333}
+                 {:host "" :port 3333}]
+          i (util/parse-int si)]
+    (if (= 0 i)
+      (bracha-broadcast addrs {:type :initial
+                               :v v
+                               :id i
+                               :r 0})
+      (bracha-broadcast addrs))
+    )))
